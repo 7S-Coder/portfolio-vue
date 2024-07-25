99fa40ff292697f97139845e90d142ee8416cfb4
7S-Coder <dylan.e33@hotmail.fr>
2024-07-25 23:23:47 +0200

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,11 +7,26 @@
 // });
 </script>
 
-<template></template>
+<template>
+  <section>
+    <img src="../assets/github.png" alt="mon github" width="25px" />
+    <span>Dernière mise à jour du site 28/07/2024</span>
+  </section>
+</template>
 
 <style scoped>
-footer {
+section {
   background-color: black;
   color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px;
+}
+
+span {
+  margin-left: 5px;
+  font-size: 0.8rem;
 }
 </style>
